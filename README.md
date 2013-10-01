bbceed5e10eeafb5e15f9bc61199f909486c5789
Dev Tsubaki <dev.tsubaki@gmail.com>
2013-10-01 17:47:56 +0900

    initialize projects

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 youtube_search_mlb
 ==================
+[YouTube API v2.0 ? API Query Parameters - YouTube ? Google Developers](https://developers.google.com/youtube/2.0/developers_guide_protocol_api_query_parameters)
---
+++ myscript.js
@@ -0,0 +1,31 @@
+﻿var mainCtrl = function($scope, $http) {
+    var today = dateFormat(new Date());
+    console.log(today);
+    
+    var url = 'https://gdata.youtube.com/feeds/api/videos?'
+        + [
+            'q=' + today + '+Recap',
+            'author=MLBGlobal13',
+            'alt=json',
+            'max-results=15',
+            'orderby=published',
+            'v=2',
+            'callback=JSON_CALLBACK'
+        ].join('&');
+        
+    $http.jsonp(url).success(function(data) {
+        $scope.results = data.feed.entry;
+    });
+}
+
+function dateFormat(date) {
+  var y = date.getFullYear();
+  var m = date.getMonth() + 1;
+  var d = date.getDate();
+
+  m = ('0' + m).slice(-2);
+  d = ('0' + d).slice(-2);
+
+  // フォーマット整形済みの文字列を戻り値にする
+  return y + '/' + m + '/' + d;
+}
