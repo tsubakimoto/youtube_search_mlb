a5d431700f8eaf1449c3e252c32c9c61bc813bea
Dev Tsubaki <dev.tsubaki@gmail.com>
2013-10-01 21:29:52 +0900

    recap

--- myscript.js
+++ myscript.js
@@ -1,10 +1,12 @@
-﻿var mainCtrl = function($scope, $http) {
+﻿var apiUrl = 'https://gdata.youtube.com/feeds/api/videos?';
+
+// dailyRecapCtrl
+var dailyRecapCtrl = function($scope, $http) {
     var today = dateFormat(new Date());
-    console.log(today);
     
-    var url = 'https://gdata.youtube.com/feeds/api/videos?'
+    var url = apiUrl
         + [
-            'q=' + today + '+Recap',
+            'q=Recap+' + today,
             'author=MLBGlobal13',
             'alt=json',
             'max-results=15',
@@ -16,16 +18,42 @@
     $http.jsonp(url).success(function(data) {
         $scope.results = data.feed.entry;
     });
-}
+};
+
+// recapCtrl
+var recapCtrl = function($scope, $http) {
+    $scope.doSearch = function() {
+        var q = encodeURIComponent($scope.date).replace('-', '/');
+        
+        if ($scope.query != null) {
+            q = q + '+' + encodeURIComponent($scope.query);
+        }
+        
+        var url = apiUrl
+            + [
+                'q=Recap+' + q,
+                'author=MLBGlobal13',
+                'alt=json',
+                'max-results=15',
+                'orderby=published',
+                'v=2',
+                'callback=JSON_CALLBACK'
+            ].join('&');
+            
+        $http.jsonp(url).success(function(data) {
+            $scope.results = data.feed.entry;
+        });
+    }
+};
 
 function dateFormat(date) {
-  var y = date.getFullYear();
-  var m = date.getMonth() + 1;
-  var d = date.getDate();
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    var d = date.getDate();
 
-  m = ('0' + m).slice(-2);
-  d = ('0' + d).slice(-2);
+    m = ('0' + m).slice(-2);
+    d = ('0' + d).slice(-2);
 
-  // フォーマット整形済みの文字列を戻り値にする
-  return y + '/' + m + '/' + d;
+    // フォーマット整形済みの文字列を戻り値にする
+    return y + '/' + m + '/' + d;
 }
